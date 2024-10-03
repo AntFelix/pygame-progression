55a12f3c4526987128ae50cc05814274d72c6ac2
AntFelix <felixantgo@gmail.com>
2024-10-03 12:16:30 +0200

    setting boundaries

--- shapes.py
+++ shapes.py
@@ -2,13 +2,13 @@ import pygame
 pygame.init()
 
 
-screen = pygame.display.set_mode((500,500))
+screen = pygame.display.set_mode((500, 500))
 screen.fill("white")
 pygame.display.set_caption("Drawing shapes on a screen")
 
-pygame.draw.line(screen, "black", (0,0),(300, 300),5)
-pygame.draw.lines(screen,"orange", True, [(100,100), (200,100), (100,200)], 4)
-pygame.draw.rect(screen, "red", (50,50,100,100),7)
+pygame.draw.line(screen, "black", (0, 0),(300, 300),5)
+pygame.draw.lines(screen, "orange", True, [(100, 100), (200, 100), (100, 200)], 4)
+pygame.draw.rect(screen, "red", (50, 50, 100, 100),7)
 pygame.draw.circle(screen, "green", (100, 100), 100, 3)
 pygame.draw.ellipse(screen, "purple", (250, 250, 100, 150), 6)
 pygame.draw.polygon(screen, "blue", ((250, 75), (300, 25), (350, 75)), 1)
